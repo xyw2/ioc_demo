8c169e36908680bb8441c2b5c4b03aeaedc371cb
xiyw <402595164@qq.com>
2020-05-10 16:38:11 +0800

    例5中加上了getBean方法的3种不同参数使用

--- src/main/java/com/xiyw/ioc_demo/ioc05/BookServiceImpl.java
+++ src/main/java/com/xiyw/ioc_demo/ioc05/BookServiceImpl.java
@@ -4,12 +4,17 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class BookServiceImpl implements BookService {
-	
+
 	private IBookDao bookDao;
-	
-	public BookServiceImpl(){
+
+	public BookServiceImpl() {
 		ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
-		bookDao = (IBookDao)ac.getBean("bookDao");
+		IBookDao bookDao1 = (IBookDao) ac.getBean("bookDao");
+		IBookDao bookDao2 = (IBookDao) ac.getBean(BookDao.class);
+		IBookDao bookDao3 = (IBookDao) ac.getBean("bookDao", BookDao.class);
+		System.out.println("bookDao1==bookDao2:" + (bookDao1 == bookDao2));
+		System.out.println("bookDao2==bookDao3:" + (bookDao2 == bookDao3));
+		bookDao = bookDao2;
 	}
 
 	public void addBook(String bookName) {
--- src/test/java/com/xiyw/ioc_demo/Test5.java
+++ src/test/java/com/xiyw/ioc_demo/Test5.java
@@ -7,9 +7,13 @@ public class Test5 {
 
 	/**
 	 * 05版本，通过applicationContext.xml配置了bean，启动时自动装入容器
-	 * 使用Spring的AppcationContext的getBean方法使用xml配置的beanId获取
+	 * 使用Spring的AppcationContext的getBean方法使用xml配置的beanId获取 
+	 * getBean(String name)使用xml文件中配置bean时用的id
+	 * getBean(Class<T> type)使用xml文件中配置bean时用的BookDao.class
+	 * getBean(String name,Class<T> type)如果name或type不唯一，可以结合在一起来保证get的bean唯一
+	 * 
 	 */
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		BookService bookservice = new BookServiceImpl();
 		bookservice.addBook("《Spring MVC权威指南 第一版》");
 	}
