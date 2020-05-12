3df3e996edc08e7bd6d2879d51ff8464d68bbcd7
xiyw <402595164@qq.com>
2020-05-12 22:17:45 +0800

    用context:property-placeholder读取的配置文件，不会加载到环境中

--- src/test/java/com/xiyw/ioc_demo/Test11.java
+++ src/test/java/com/xiyw/ioc_demo/Test11.java
@@ -1,6 +1,8 @@
 package com.xiyw.ioc_demo;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.Environment;
 
 public class Test11 {
 
@@ -9,7 +11,10 @@ public class Test11 {
 	 * 用@value("${}")来获取属性文件的值，可用于数据库连接等实例在启动时的新建
 	 */
 	public static void main(String[] args) {
-		new ClassPathXmlApplicationContext("applicationContext11.xml");
+		ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext11.xml");
+		// 用context:property-placeholder读取的配置文件，不会加载到环境中
+		Environment environment = ac.getEnvironment();
+		System.out.println("environment.getProperty('db.driverClass')=" + environment.getProperty("db.driverClass"));
 	}
 
 }
